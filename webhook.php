79d2f9e2c5fda948733356ccd721634ef2a26c62
chaiyanzhi <18536661249@163.com>
2020-03-13 22:36:12 +0800

    15

--- webhook.php
+++ webhook.php
@@ -1,10 +1,6 @@
 <?php
 
 $pwd = getcwd();
-<<<<<<< HEAD
-=======
-// $pwd = 'test';
->>>>>>> 005d6b46a0f2e531db85320e6d5887887f01aa66
 
 $command = 'cd ' . $pwd . ' && git pull';
 $output = shell_exec($command);
