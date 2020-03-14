d7cac76067d82e3371249afb1a564f8e95def96a
chaiyanzhi <18536661249@163.com>
2020-03-14 10:18:03 +0800

    1005

--- webhook.php
+++ webhook.php
@@ -5,10 +5,10 @@ $pwd = getcwd();
 $command = 'cd ' . $pwd . ' && git pull';echo $command;
 $output = exec('cd /www/wwwroot/www.luckymessenger.xyz && git pull');
 echo $output;
-print_r(get_current_user());
+var_dump(get_current_user());
     $cmd = 'php -v';
     exec($cmd, $arr);
-    echo '<pre>'
-    print_r($arr);
+    echo '<pre>';
+    var_dump($arr);
 
 ?>
